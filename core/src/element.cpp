9c37518993f9cea0504115b68bae9486d59ea213
Potaty Code <Potatyyman@proton.me>
2025-11-22 16:19:02 +1000

    added basic tiling functionality

--- core/src/element.cpp
+++ core/src/element.cpp
@@ -3,7 +3,19 @@
 
 namespace core {
 // Core initialization
-void element::tileChildren() {}
+void element::tileChildren() {
+  for (auto &child : childElements) {
+    child->position = calcPosition();
+    child->tileChildren();
+  }
+}
 
-raylib::Vector2 element::calcPosition() { raylib::Vector2 newPosition; }
+raylib::Vector2 element::calcPosition() {
+  raylib::Vector2 newPosition;
+  if (Parent->childrenHorizental) {
+    newPosition.x = dependentElement->position.x + padding.x;
+    newPosition.y = Parent->position.y + padding.y;
+  }
+  return newPosition;
+}
 } // namespace core
