3d330c49056a385951a0730832430dcd261ba522
Potaty Code <Potatyyman@proton.me>
2025-11-23 11:32:37 +1000

    added clang format and tidy and adjusted code to fit

--- core/src/element.cpp
+++ core/src/element.cpp
@@ -1,21 +1,22 @@
 #include "../include/element.hpp"
-#include <Vector2.hpp>
 
 namespace core {
 // Core initialization
-void element::tileChildren() {
-  for (auto &child : childElements) {
-    child->position = calcPosition();
-    child->tileChildren();
-  }
+Element::Element() {}
+void Element::tile_children() {
+    for (auto& child : childElements_) {
+        child->position_ = calc_position();
+        child->tile_children();
+    }
 }
 
-raylib::Vector2 element::calcPosition() {
-  raylib::Vector2 newPosition;
-  if (Parent->childrenHorizental) {
-    newPosition.x = dependentElement->position.x + padding.x;
-    newPosition.y = Parent->position.y + padding.y;
-  }
-  return newPosition;
+raylib::Vector2 Element::calc_position() {
+    raylib::Vector2 new_position;
+    if (parent_->is_children_horizontal()) {
+        new_position.x = dependentElement_->position_.x + padding_.x;
+        new_position.y = parent_->position_.y + padding_.y;
+    }
+    return new_position;
 }
-} // namespace core
+raylib::Vector2 Element::update_dimension() {}
+}  // namespace core
